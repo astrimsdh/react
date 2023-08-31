008ab08be79c85e5c259bc697cdc0baa37be0ec2
Astri Musidah <astry.musidah@gmail.com>
2023-08-31 14:03:48 +0700

    Menyelesaikan pembuatan bagian footer dan merapikan strukur dari react memisahkan tiap komponen menjadi file terpisan

--- 4-catatan-belanja/src/App.jsx
+++ 4-catatan-belanja/src/App.jsx
@@ -1,4 +1,9 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
+import Header from './components/Header';
+import Form from './components/Form';
+import GroceryList from './components/GroceryList';
+import Footer from './components/Footer';
 
 const groceryItems = [
   {
@@ -45,101 +50,16 @@ export default function App() {
       <Header />
       <Form onAddItem={handleAddItem}/>
       <GroceryList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} onClearItems={handleClearItems}/>
-      <Footer />
+      <Footer items={items}/>
     </div>
   )
 }
 
-function Header() {
-  return <h1>Catatan Belanjaku 📝</h1>;
-}
 
-function Form({onAddItem}) {
-  const [name, setName] = useState('');
-  const [quantity, setQuantity] = useState(1);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    if(!name) return;
-    const newItem = {
-      name, 
-      quantity,
-      checked: false,
-      id: Date.now()
-    }
-    onAddItem(newItem);
-    setName('');
-    setQuantity(1);
-  }
 
-  const quantityNum = [...Array(20)].map((_, i) => (
-    <option value={i + 1} key={i + 1}>
-      {i + 1}
-    </option>
-  ));
 
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-        <h3>Hari ini belanja apa kita?</h3>
-        <div>
-          <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>{quantityNum}</select>
-          <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)}/>
-        </div>
-        <button>Tambah</button>
-      </form>
-  );
-}
 
-function GroceryList({items, onDeleteItem, onToggleItem, onClearItems}) {
-  const [sortBy, setSortBy] = useState('input');
-  let sortedItems;
 
-  switch(sortBy){
-    case 'name' :
-      sortedItems = items.slice().sort((a, b) => a.name.localeCompare(b.name));
-      break;
-    case 'checked' :
-      sortedItems = items.slice().sort((a, b) => a.checked - b.checked);
-      break;
-    default :
-      sortedItems = items;
-      break;
-  }
-  
-  return (
-    <>
-    <div className="list">
-      <ul>
-        {
-          sortedItems.map((item) => (
-            <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>
-          ))
-        }
-        
-      </ul>
-    </div>
-    <div className="actions">
-      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-        <option value="input">Urutkan berdasarkan urutan input</option>
-        <option value="name">Urutkan berdasarkan nama barang</option>
-        <option value="checked">Urutkan berdasarkan ceklis</option>
-      </select>
-      <button onClick={onClearItems}>Bersihkan Daftar</button>
-    </div>
-    </>
-  );
-}
 
-function Item({item, onDeleteItem, onToggleItem}) {
-  return (
-    <li key={item.id}>
-      <input type="checkbox" checked={item.checked} onChange={() => onToggleItem(item.id)}/>
-      <span style={item.checked ? {textDecoration: 'line-through'} : {}}>{item.quantity} {item.name}</span>
-      <button onClick={() => onDeleteItem(item.id)}>&times;</button>
-    </li>
-  );
-}
 
-function Footer() {
-  return <footer className="stats">Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)</footer>;
-}
