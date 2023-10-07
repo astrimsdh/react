540fc0a141b8d2489e908cb8606d606d26ed6e13
Astri Musidah <astry.musidah@gmail.com>
2023-10-07 14:41:10 +0700

    Membuat reusable box, dengan menggunakan komponen composite untuk menyelesaikan permasalahan prop drilling

--- anime-app/src/App.jsx
+++ anime-app/src/App.jsx
@@ -42,22 +42,37 @@ const animesData = [
 
 export default function App() {
   const [animes, setAnimes] = useState(animesData);
-  
+  const [selectedAnime, setSelectedAnime] = useState(animes[0]);
 
+  function handleSelectedAnime(id) {
+    const newAnime = animes.filter((anime) => anime.mal_id === id);
+    setSelectedAnime(newAnime[0]);
+  }
   return (
     <>
-      <NavBar animes={animes} />
-      <Main animes={animes}/>
+      <NavBar>
+        <Search>
+          <NumResult animes={animes} />
+        </Search>
+      </NavBar>
+      <Main animes={animes}>
+        <Box>
+          <AnimeList animes={animes} onSelectedAnime={handleSelectedAnime}/>
+        </Box>
+        <Box>
+          <AnimeDetail selectedAnime={selectedAnime}/>
+        </Box>
+      </Main>
     </>
   );
 }
 
-function NavBar({animes}) {
+function NavBar({children}) {
 
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search animes={animes}/>
+      {children}
     </nav>
   );
 }
@@ -72,14 +87,14 @@ function Logo() {
   );
 }
 
-function Search({animes}) {
+function Search({children}) {
   const [query, setQuery] = useState('');
 
   return (
     <div className="search-container">
-          <input className="search" type="text" placeholder="Search anime..." value={query} onChange={(e) => setQuery(e.target.value)} />
-          <NumResult animes={animes}/>
-        </div>
+      <input className="search" type="text" placeholder="Search anime..." value={query} onChange={(e) => setQuery(e.target.value)} />
+      {children}
+    </div>
   );
 }
 
@@ -91,29 +106,23 @@ function NumResult({animes}) {
   );
 }
 
-function Main({animes}) {
-  const [selectedAnime, setSelectedAnime] = useState(animes[0]);
-
-  function handleSelectedAnime(id) {
-    const newAnime = animes.filter((anime) => anime.mal_id === id);
-    setSelectedAnime(newAnime[0]);
-  }
+function Main({children}) {
+  
   return (
     <main className="main">
-        <ListBox animes={animes} onSelectedAnime={handleSelectedAnime}/>
-        <SelectedBox selectedAnime={selectedAnime}/>
-      </main>
+      {children}
+    </main>
   );
 }
 
-function ListBox({animes, onSelectedAnime}){
-  const [isOpen1, setIsOpen1] = useState(true);
+function Box({children}){
+  const [isOpen, setIsOpen1] = useState(true);
   return (
     <div className="box">
           <button className="btn-toggle" onClick={() => setIsOpen1((open) => !open)}>
-            {isOpen1 ? '–' : '+'}
+            {isOpen ? '–' : '+'}
           </button>
-          {isOpen1 && <AnimeList animes={animes} onSelectedAnime={onSelectedAnime}/>}
+          {isOpen && children}
         </div>
   );
 }
@@ -139,18 +148,6 @@ function Anime({anime, onSelectedAnime}){
         </p>
       </div>
     </li>
-  );
-}
-
-function SelectedBox({selectedAnime}){
-  const [isOpen2, setIsOpen2] = useState(true);
-  return (
-    <div className="box">
-          <button className="btn-toggle" onClick={() => setIsOpen2((open) => !open)}>
-            {isOpen2 ? '–' : '+'}
-          </button>
-          {isOpen2 && <AnimeDetail selectedAnime={selectedAnime}/>}
-        </div>
   );
 }
 
